38900c1e44b4d52b75194d50425c5bc1847f9991
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-10-20 12:42:13 +0200

    neu

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import 'bootstrap';
+import SimpleLightBox from 'simplelightbox';
+
+import './scss/custom.scss';
+
+let gallery = new SimpleLightBox('.gallery a');
+gallery.on('show.simplelightbox', function() {
+    
+});
